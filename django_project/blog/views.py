1f0b4a747f71061009702a3d829c58086b7f2a9c
emanmacario <emacario1997@hotmail.com>
2020-08-29 17:16:25 +1000

    Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# Mock DB data
+posts = [
+    {
+        'author': 'Emmanuel Macario',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 29, 2020'
+    },
+    {
+        'author': 'Hagrid',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 12, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
