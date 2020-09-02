f74ae743575d41ce71317baffb6f0351adf82bab
emanmacario <emacario1997@hotmail.com>
2020-09-02 15:43:03 +1000

    Add database and migrations

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Each class is own table in database
+# Each attribute is different field in database
+
+# To make migrations, view SQL and run migrations:
+# $ python manage.py makemigrations
+# $ python manage.py sqlmigrate blog 0001
+# $ python manage.py migrate
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Mock DB data
-posts = [
-    {
-        'author': 'Emmanuel Macario',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 29, 2020'
-    },
-    {
-        'author': 'Hagrid',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 12, 2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
