4d91492950caf398610b6fb441f4aa37c8d757a8
emanmacario <emacario1997@hotmail.com>
2020-09-03 15:09:47 +1000

    Update user profile

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        """
+        Overrides save method of Profile model, resizes user profile pictures
+        """
+        super().save()
+
+        im = Image.open(self.image.path)
+
+        if im.height > 300 or im.width > 300:
+            output_size = (300, 300)
+            im.thumbnail(output_size, Image.ANTIALIAS)
+            im.save(self.image.path)
