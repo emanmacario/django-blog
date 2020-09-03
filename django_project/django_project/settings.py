294910c6e8fdc7d709870180a6391361d05b70e6
emanmacario <emacario1997@hotmail.com>
2020-09-03 19:59:17 +1000

    Password reset

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -129,3 +129,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'  # Default CSS Framework for forms
 
 LOGIN_REDIRECT_URL = 'blog-home'  # URL redirected to after login
 LOGIN_URL = 'login'               # Redirect URL for authenticated routes
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
